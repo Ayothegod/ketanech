a5ae2bef40764f5048ba80a7de432f8fddd104a2
Ayo <heyayomideadebisi@gmail.com>
2023-06-13 14:34:48 +0100

    add react icons

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,20 @@
+import Link from "next/link"
 
 const Header = () => {
   return (
-    <div>
-      <p>Lets build this shit</p>
+    <div className="flex relative">
+      <span>
+        KTS.
+      </span>
+      <nav className="absolute inset-0 h-40">
+        <ul>
+          <Link href="/"><li>Home</li></Link>
+          <Link href="/"><li>About</li></Link>
+          <Link href="/"><li>Services</li></Link>
+          <Link href="/"><li>Contact</li></Link>
+          <Link href="/"><li>Training</li></Link>
+        </ul>
+      </nav>
     </div>
   )
 }
