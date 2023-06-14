ba99b6bb25119e0a7eef516f55ca9e5dfb978ebc
Ayo <heyayomideadebisi@gmail.com>
2023-06-14 02:11:54 +0100

    initial wghatwedo

--- components/Body.jsx
+++ components/Body.jsx
@@ -1,9 +1,11 @@
 import Mission from "./Mission"
+import WhatWeDo from "./WhatWeDo"
 
 const Body = () => {
   return (
     <div className="max-w-[50rem] mx-auto p-2">
       <Mission/>
+      <WhatWeDo/>
     </div>
   )
 }
