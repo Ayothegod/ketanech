4d0da6c1aa4a61ec4d3ec182ff4ff234a8884889
Ayo <heyayomideadebisi@gmail.com>
2023-06-14 13:49:04 +0100

    add initial whatwedo

--- components/Body.jsx
+++ components/Body.jsx
@@ -3,7 +3,7 @@ import WhatWeDo from "./WhatWeDo"
 
 const Body = () => {
   return (
-    <div className="max-w-[50rem] mx-auto p-2">
+    <div className="">
       <Mission/>
       <WhatWeDo/>
     </div>
--- components/WhatWeDo.jsx
+++ components/WhatWeDo.jsx
@@ -1,33 +1,48 @@
 import james4 from "@/asset/james-kovin-qqLxF3M-MA8-unsplash-4.jpg"
 import nuno4 from "@/asset/nuno-marques-0GbrjL3vZF4-unsplash-4.jpg"
 import sidney4 from "@/asset/sidney-pearce-WRgKwttuP7A-unsplash-4.jpg"
+import ahsanization4 from "@/asset/ahsanization-mJi2I9KJPQ8-unsplash-4.jpg"
+import gower4 from "@/asset/christopher-gower-m_HRfLhgABo-unsplash-4.jpg"
 import Image from "next/image"
 
 const WhatWeDo = () => {
     return (
-        <div className="mt-8">
+        <div className="mt-8 mb-16 max-w-[70rem] mx-auto p-2">
             <h2 className="grid place-items-center text-xl text-blue font-semibold uppercase text-center mb-2">What we do.</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-2">
+            <div className="grid grid-cols-1 sm:grid-cols-3 gap-2">
                 <div className=" relative" >
-                    <Image src={james4} alt="first" className="h-40 w-full object-cover object-center" />
+                    <Image src={james4} alt="first" className="h-24 sm:h-40 w-full object-cover object-center" />
                     <div className="absolute inset-0 bg-[rgba(0,0,0,.4)] grid place-items-center">
                         <p className="text-xl font-bold text-white">Electrical and Electronics</p>
                     </div>
                 </div>
 
                 <div className=" relative">
-                    <Image src={nuno4} alt="absolute" className=" h-40 w-full" />
+                    <Image src={nuno4} alt="absolute" className="h-24 sm:h-40 w-full object-cover object-center" />
                     <div className="absolute inset-0 bg-[rgba(0,0,0,.4)] grid place-items-center">
                         <p className="text-xl font-bold text-white">Industrial Automation</p>
                     </div>
                 </div>
 
                 <div className=" relative">
-                    <Image src={sidney4} alt="absolute" className=" h-40 w-full" />
+                    <Image src={sidney4} alt="absolute" className="h-24 sm:h-40 w-full object-cover object-center" />
                     <div className="absolute inset-0 bg-[rgba(0,0,0,.4)] grid place-items-center">
                         <p className="text-xl font-bold text-white">Fabrication</p>
                     </div>
                 </div>
+                <div className=" relative">
+                    <Image src={ahsanization4} alt="absolute" className="h-24 sm:h-40 w-full object-cover object-center" />
+                    <div className="absolute inset-0 bg-[rgba(0,0,0,.4)] grid place-items-center">
+                        <p className="text-xl font-bold text-white">Outdoor Services</p>
+                    </div>
+                </div>
+                <div className=" relative">
+                    <Image src={gower4} alt="absolute" className="h-24 sm:h-40 w-full object-cover object-center" />
+                    <div className="absolute inset-0 bg-[rgba(0,0,0,.4)] grid place-items-center">
+                        <p className="text-xl font-bold text-white">Software/Embeded systems</p>
+                    </div>
+                </div>
+
             </div>
         </div>
     )
