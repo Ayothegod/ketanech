18086239743877b6dfa015584eeb2632a94a5b12
Ayo <heyayomideadebisi@gmail.com>
2023-09-19 16:58:13 +0100

    try chatgpt

--- service/mailService.js
+++ service/mailService.js
@@ -1,14 +1,43 @@
 var nodemailer = require("nodemailer");
+const { google } = require("googleapis")
 
+// Set up OAuth2 credentials
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URL
+);
+
+oauth2Client.setCredentials({
+    refresh_token: 'MY_REFRESH_TOKEN',
+});
 
 export async function sendMail(fromEmail, subject, otpText) {
-  var transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.NODEMAILER_EMAIL,
-      pass: process.env.NODEMAILER_PASSWORD,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    type: 'OAuth2',
+    user: 'your.email@gmail.com',
+    clientId: 'YOUR_CLIENT_ID',
+    clientSecret: 'YOUR_CLIENT_SECRET',
+    refreshToken: 'YOUR_REFRESH_TOKEN',
+    accessToken: oauth2Client.getAccessToken(),
+  },
+});
+
+// const mailOptions = {
+//   from: 'your.email@gmail.com',
+//   to: 'recipient@example.com',
+//   subject: 'Subject',
+//   text: 'Message body',
+// };
+// transporter.sendMail(mailOptions, (error, info) => {
+//   if (error) {
+//     console.error(error);
+//   } else {
+//     console.log('Email sent: ' + info.response);
+//   }
+// });
 
   var mailOptions = {
     from: fromEmail,
@@ -16,15 +45,6 @@ export async function sendMail(fromEmail, subject, otpText) {
     subject: subject,
     text: otpText,
   };
-
-//   transporter.sendMail(mailOptions, function (error, info) {
-//     if (error) {
-//       throw new Error(error);
-//     } else {
-//       console.log("Email Sent");
-//       return true;
-//     }
-//   });
 
 await new Promise((resolve, reject) => {
     // send mail
@@ -37,3 +57,31 @@ await new Promise((resolve, reject) => {
     });
   });
 }
+
+// export async function sendMail(fromEmail, subject, otpText) {
+//     var transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.NODEMAILER_EMAIL,
+//         pass: process.env.NODEMAILER_PASSWORD,
+//       },
+//     });
+  
+//     var mailOptions = {
+//       from: fromEmail,
+//       to: process.env.NODEMAILER_EMAIL,
+//       subject: subject,
+//       text: otpText,
+//     };
+  
+//   await new Promise((resolve, reject) => {
+//       // send mail
+//       transporter.sendMail(mailOptions, (err, response) => {
+//         if (err) {
+//           reject(err);
+//         } else {
+//           resolve(response);
+//         }
+//       });
+//     });
+//   }
